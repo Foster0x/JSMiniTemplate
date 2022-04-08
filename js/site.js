76bce9b0acdd2b47a7e9bdd58a3c3ee14d9b25a9
Foster0x <sfoster2010.sf@gmail.com>
2022-04-08 12:56:07 -0400

    display message and js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
-function displayMessage() {
-    alert("Hello Stephen Foster");
+// Sep One - controller/accepts requests
+function getMessage() {
+    let msg = document.getElementById("txtMessage").value;
+    displayMessage(msg);
+}
+
+// Final Step - view
+function displayMessage(message) {
+    let msg = `${message} Stephen Foster`; 
+    element = document.getElementById("results");
+    element.innerHTML = msg;
 }
